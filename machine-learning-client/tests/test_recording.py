4fdd9a02481963c0db62b965eb5814f9f7a5ae2d
JennyShen10792 <js10792@nyu.edu>
2022-12-04 18:51:24 -0500

    Fix tests and add dependency

--- machine-learning-client/tests/test_recording.py
+++ machine-learning-client/tests/test_recording.py
@@ -2,23 +2,24 @@ import os
 from os.path import dirname, join
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import speech-recognition
+
+import speech
 
 class Tests:
 		
 	def test_1(self):
-		output = speech-recognition.AudioText(1, 'werent-you-listening.wav')
+		output = speech.AudioText(1, 'werent-you-listening.wav')
 		assert output == "weren't you listening"
 		
 	def test_2(self):
-		output = speech-recognition.AudioText(1, 'we-need-to-have.wav')
+		output = speech.AudioText(1, 'we-need-to-have.wav')
 		assert output == "we need to have a serious talk"
 	
 	def test_3(self):
-		output = speech-recognition.AudioText(1, 'youre-on-the-right-track.wav')
+		output = speech.AudioText(1, 'youre-on-the-right-track.wav')
 		assert output == "you're on the right track"
 	
 	def test_background_noise(self):
-		output = speech-recognition.AudioText(1, 'this-part-of-my-life-this-little-part-is-called-happiness.wav')
+		output = speech.AudioText(1, 'this-part-of-my-life-this-little-part-is-called-happiness.wav')
 		assert output == "this part of my life this little part is called happiness"
 	
