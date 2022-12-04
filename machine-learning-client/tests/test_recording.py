4a3594a5fe9b42ac1b60d9268a1b5a636368fae6
JennyShen10792 <js10792@nyu.edu>
2022-12-04 18:21:42 -0500

    Fix tests

--- machine-learning-client/tests/test_recording.py
+++ machine-learning-client/tests/test_recording.py
@@ -1,4 +1,7 @@
-import pytest
+import os
+from os.path import dirname, join
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import speech-recognition
 
 class Tests:
