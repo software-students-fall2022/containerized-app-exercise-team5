bd02e66f2aa4d1def47a48bc88a815fc4e8b8b1c
ian-Liaozy <zl3057@nyu.edu>
2022-12-04 21:46:30 -0500

    connect to db, works well

--- db/init-mongo.js
+++
@@ -1,8 +0,0 @@
-db = db.getSiblingDB("sentiments");
-
-db.createCollection('containerizedTest');
-
-db.sentiments.insertMany([
-    {text: 'I am so happy and sad', isPositive: true, positive_words: ['happy', 'happy2'], negative_words: ['sad', 'sad2'], createdDate: '2022-12-01'},
-    {text: 'I am so happy and sad sad', isPositive: false, positive_words: ['happy', 'happy2'], negative_words: ['sad', 'sad2'], createdDate: '2022-12-02'}
-]);
