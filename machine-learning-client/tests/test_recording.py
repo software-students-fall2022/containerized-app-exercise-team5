e498a25a0f83aafc36738a6d157caee56df3c2aa
Victoria <rz1424@nyu.edu>
2022-12-04 19:31:02 -0500

    add path

--- machine-learning-client/tests/test_recording.py
+++ machine-learning-client/tests/test_recording.py
@@ -5,24 +5,24 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import speech
 
-sys.path.append("/test")
+# sys.path.append("/test")
 
 class Tests:
 		
 	def test_1(self):
-		print(sys.path)
-		output = speech.AudioText(1, 'werent-you-listening.wav')
+		# print(sys.path)
+		output = speech.AudioText(1, '/tests/werent-you-listening.wav')
 		assert output == "weren't you listening"
 		
 	def test_2(self):
-		output = speech.AudioText(1, 'we-need-to-have.wav')
+		output = speech.AudioText(1, '/tests/we-need-to-have.wav')
 		assert output == "we need to have a serious talk"
 	
 	def test_3(self):
-		output = speech.AudioText(1, 'youre-on-the-right-track.wav')
+		output = speech.AudioText(1, '/tests/youre-on-the-right-track.wav')
 		assert output == "you're on the right track"
 	
 	def test_background_noise(self):
-		output = speech.AudioText(1, 'this-part-of-my-life-this-little-part-is-called-happiness.wav')
+		output = speech.AudioText(1, '/tests/this-part-of-my-life-this-little-part-is-called-happiness.wav')
 		assert output == "this part of my life this little part is called happiness"
 	
